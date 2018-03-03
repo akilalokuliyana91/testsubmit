af9a3806af8ee5dbc8f79b9e86460cfac4de86b3
akila9173 <akilajanith123@gmail.com>
2018-03-03 21:25:01 +0530

    test commit

---
+++ TestProject/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestProject.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public string CustomerAddress { get; set; }
+
+        public string CustomerNumber { get; set; }
+    }
+}
---
+++ TestProject/Models/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestProject.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+
+        public string ItemName { get; set; }
+
+        public string ItemDescription { get; set; }
+
+        public string Qty { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public Customer Customer { get; set; }
+    }
+}
